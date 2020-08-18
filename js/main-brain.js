dcee6bd8014ba90f1170f54527d5399a91f14c65
Jack-Lupino <50802230+Jack-Lupino@users.noreply.github.com>
2020-08-19 01:04:47 +0300

    test jQuery's load()

--- js/main-brain.js
+++ js/main-brain.js
@@ -49,6 +49,10 @@ customElements.define('main-header', Header);
 customElements.define('main-footer', Footer); 
 customElements.define('main-meta', MetaHTML);
 
+$(function() {
+	$("#inject").load('templates/header.html');
+});
+
 function printTime(){
 	let date = new Date();
 	$("#time").html(date.toLocaleTimeString() + ", " + date.toLocaleDateString());
