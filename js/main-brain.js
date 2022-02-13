79fc54c7296ff2fa999bfd142c14e5c59872d0d7
Jack-Lupino <somniterra.vf@gmail.com>
2022-02-13 18:28:08 +0200

    test JS element definition

--- js/main-brain.js
+++ js/main-brain.js
@@ -1,53 +1,53 @@
-// class MetaHTML extends HTMLElement {
-// 	connectedCallback() {
-// 		this.innerHTML = `
-// 		<meta charset="UTF-8">
-// 		<link rel="stylesheet" href="css/main-style.css">
-// 		<link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC" rel="stylesheet"> 
-// 		<link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">
-// 		`;
-// 	}
-// }
-// class Header extends HTMLElement {
-// 	connectedCallback() {
-// 	  this.innerHTML = `
-// 	  <p id="time">00:00:00, 0000/00/00</p>
-// 	  <header class="section">
-// 		  <h1>Project Solo-1</h1>
-// 	  </header>
-// 	  <hr>
-// 	  <nav>
-// 		  <ul>
-// 			  <li><a href="index.html">Home</a></li>
-// 			  <li><a href="Literature.html">Literature</a></li>
-// 			  <li><a href="Project-Euler.html">Project Euler</a></li>
-// 			  <li><a href="Features.html">Features</a></li>
-// 			  <li><a href="Cryptography.html">Cryptography</a></li>
-// 			  <li><a href="Reference.html">Reference</a></li>
-// 		  </ul>
-// 	  </nav>
-// 	  <hr>
-// 	  `;
-// 	}
-//   }
-// class Footer extends HTMLElement {
-// 	connectedCallback() {
-// 	  this.innerHTML = `	   
-// 	  <hr>
-// 	  <footer>
-// 		  <a href="index.html">Home</a></li>
-// 		  <a href="Literature.html">Literature</a>
-// 		  <a href="Project-Euler.html">Project Euler</a>
-// 		  <a href="Test-page.html">Features</a>
-// 		  <a href="Cryptography.html">Cryptography</a>
-// 		  <a href="Reference.html">Reference</a>
-// 	  </footer> 
-// 	  `;
-// 	}
-// }
-// customElements.define('main-header', Header);
-// customElements.define('main-footer', Footer); 
-// customElements.define('main-meta', MetaHTML);
+class MetaHTML extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/main-style.css">
+		<link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC" rel="stylesheet"> 
+		<link rel="shortcut icon" type="image/x-icon" href="media/favicon.ico">
+		`;
+	}
+}
+class Header extends HTMLElement {
+	connectedCallback() {
+	  this.innerHTML = `
+	  <p id="time">00:00:00, 0000/00/00</p>
+	  <header class="section">
+		  <h1>Project Solo-1</h1>
+	  </header>
+	  <hr>
+	  <nav>
+		  <ul>
+			  <li><a href="index.html">Home</a></li>
+			  <li><a href="Literature.html">Literature</a></li>
+			  <li><a href="Project-Euler.html">Project Euler</a></li>
+			  <li><a href="Features.html">Features</a></li>
+			  <li><a href="Cryptography.html">Cryptography</a></li>
+			  <li><a href="Reference.html">Reference</a></li>
+		  </ul>
+	  </nav>
+	  <hr>
+	  `;
+	}
+  }
+class Footer extends HTMLElement {
+	connectedCallback() {
+	  this.innerHTML = `	   
+	  <hr>
+	  <footer>
+		  <a href="index.html">Home</a></li>
+		  <a href="Literature.html">Literature</a>
+		  <a href="Project-Euler.html">Project Euler</a>
+		  <a href="Test-page.html">Features</a>
+		  <a href="Cryptography.html">Cryptography</a>
+		  <a href="Reference.html">Reference</a>
+	  </footer> 
+	  `;
+	}
+}
+customElements.define('main-header', Header);
+customElements.define('main-footer', Footer); 
+customElements.define('main-meta', MetaHTML);
 
 $(function() {
 	$("#inject").load('templates/header.html');
